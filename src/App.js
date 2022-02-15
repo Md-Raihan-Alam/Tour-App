b2234a3f9f4dc1b1983319495ccd646ac2fa9a7a
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-02-15 20:39:09 +0600

    Tour App
    Tour project by react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Loading from './loading'
+import Error from './Error'
+import Tours from './Tours'
+const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [loading,setLoading]=React.useState(true);
+  const [error,setError]=React.useState(false);
+  const [tours,setTours]=React.useState([]);
+  const removeTour=(id)=>{
+    const newTours=tours.filter((tour)=>tour.id!==id);
+    setTours(newTours);
+  }
+  const fetchTours=async()=>{
+    setLoading(true);
+    try{
+      const response=await fetch(url);
+      const tours=await response.json();
+      setLoading(false);
+      setTours(tours);
+      setError(false);
+    }catch(error){
+      console.log(error);
+      setLoading(false);
+      setError(true);
+    }
+  }
+  React.useEffect(()=>{
+    fetchTours();
+  },[]);
+  if(loading){
+    return<main>
+      <Loading/>
+    </main>
+  }
+  if(error){
+    return(
+      <main>
+        <Error/>
+      </main>
+    );
+  }
+if(tours.length===0){
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button onClick={fetchTours} className="btn">Refresh</button>
+        </div>
+      </main>
+    )
+  }
+  return(
+    <main>
+    <Tours tours={tours} removeTour={removeTour}/>
+    </main>
   );
 }
 
---
+++ src/Error.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Error=()=>{
+    return(
+        <div className='loading'>
+            <h1>Error....</h1>
+        </div>
+    );
+}
+export default Error;
